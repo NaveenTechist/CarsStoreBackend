61f4f6fad5499fbf556a0c3386183058eeda6eea
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-08-18 08:07:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
