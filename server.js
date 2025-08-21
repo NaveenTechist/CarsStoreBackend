18f95e83fc3d10b6d2b4f07b7b3d7d8d420e017d
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-08-21 20:48:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI)
 // -----------------------------
 // User Schema
 // -----------------------------
-const carSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: String,
   imgUrl: String,
   rating: Number,
